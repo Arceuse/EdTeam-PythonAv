e60563be844beedde48736e7d98a8934da1cc49b
ethanarre <ethansarredondo@gmail.com>
2024-02-17 01:23:29 -0500

    clase 2.5 completa

--- modulo2/clase5/clase5.py
+++ modulo2/clase5/clase5.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 
 def get_image(image_file):
@@ -7,11 +9,32 @@ def get_image(image_file):
         print(image.size)
         print(image.mode)
         print(image.format)
-        image.show()
 
+        """
+        image.show()
         image_blackwhite = image.convert('L')
         image_blackwhite.show()
         image_blackwhite.save('miku_bn.jpg')
+        """
+
+        font = ImageFont.truetype('fonts/Roboto-Bold.ttf', 120)
+        draw = ImageDraw.Draw(image)
+        draw.text(
+            (500, 0),
+            "EDCAMP 2022",
+            (255, 255, 255),
+            font
+        )
+        image.show()
+        image.save('edcamp22_font.jpg')
+
+        """
+        create thumbnail
+        """
+
+        image.thumbnail((500, 500))
+        image.show()
+        image.save('edcamp22_thumbnail.jpg')
 
     except Exception as e:
         print('no se encontro la imagen: ', e)
